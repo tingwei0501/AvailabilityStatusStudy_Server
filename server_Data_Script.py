74ded5289e95f33cd588ad642db823c0b15fe307
tingwei0501 <tingwei0501@gmail.com>
2019-07-17 15:54:52 +0800

    add add query dump data

--- server_Data_Script.py
+++ server_Data_Script.py
@@ -9,6 +9,23 @@ app.config['MONGO_URI'] = 'mongodb://localhost/statusStudy'
 
 mongo = PyMongo(app)
 
+@app.route('/checkDumpData', methods=['POST'])
+def checkDumpData():
+    jsonquery = request.get_json(force=True, silent=True)
+    query = json.loads(jsonquery)
+    user_id = query['id']
+
+    start = datetime.datetime(2019, query['query_start_month'], query['query_start_date']-1, 16)
+    end = datetime.datetime(2019, query['query_end_month'], query['query_end_date'], 15, 59, 59)
+
+    collection = mongo.db.dump
+    dumpDataCoount = collection.find({'id': user_id, 'createdTime': {'$gte': start.timestamp()*1000, '$lt': end.timestamp()*1000} }).count()
+    
+    message = {'count': dumpDataCoount}
+    return json.dumps(message)
+
+
+
 @app.route('/checkContactStatusRate', methods=['POST'])
 def checkContactStatusRate():
     jsonquery = request.get_json(force=True, silent=True)
@@ -18,8 +35,8 @@ def checkContactStatusRate():
     start = datetime.datetime(2019, query['query_start_month'], query['query_start_date']-1, 16)
     end = datetime.datetime(2019, query['query_end_month'], query['query_end_date'], 15, 59, 59)
 
-    print (start.timestamp()*1000)
-    print (end.timestamp()*1000)
+    # print (start.timestamp()*1000)
+    # print (end.timestamp()*1000)
     # print (datetime.datetime(2019, 7, 5, 16, 0, 0).timestamp()*1000)
     # print (datetime.datetime(2019, 7, 6, 15, 59, 59).timestamp()*1000)
 
@@ -42,7 +59,8 @@ def checkContactStatusRate():
 @app.route('/notificationCompletedRate', methods=['POST'])
 def notificationCompletedRate():
     total = 0
-    completed = 0
+    selfCompleted = 0
+    selfEdit = 0
 
     jsonquery = request.get_json(force=True, silent=True)
     query = json.loads(jsonquery)
@@ -54,23 +72,28 @@ def notificationCompletedRate():
     # print (end.timestamp())
 
     collection = mongo.db.selfQuestionnaire
-    totalNotifications = collection.find({'id': user_id, 'notificationSendTime': {'$gte': start.timestamp()*1000, '$lt': end.timestamp()*1000} })
-    for item in totalNotifications:
-        # if it has created time: user complete the notification
-        total += 1
-        if 'createdTime' in item:
-            timePeriodInSec = (item['createdTime'] - item['notificationSendTime']) / 1000
+    totalQuestionnaires = collection.find({'id': user_id, 'createdTime': {'$gte': start.timestamp()*1000, '$lt': end.timestamp()*1000} })
+    for item in totalQuestionnaires:
+        if 'completeTime' not in item:
+            total += 1
+        else if 'completeTime' in item:
+            timePeriodInSec = (item['completeTime'] - item['createdTime']) / 1000
             # <= 2 hours
             if timePeriodInSec <= 7200:
-                completed += 1
+                if 'idealShowDifferent' in item:
+                    selfCompleted += 1
+                else:
+                    selfEdit += 1      
 
     print ("total: ", total)
-    print ("completed: ", completed)
+    print ("selfCompleted: ", selfCompleted)
+    print ("selfEdit: ", selfEdit)
+
     if total>0:
-        rate = completed/total
+        rate = selfCompleted/total
     else:
         rate = 0
-    message = {'total': total, 'completed': completed, 'completedRate': rate}
+    message = {'total': total, 'selfCompleted': selfCompleted, 'completedRate': rate, 'selfEditCompleted': selfEdit}
 
     return json.dumps(message)
 
